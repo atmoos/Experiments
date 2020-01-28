040123914393f0aa99e9e8639e865a018465b637
Thomas Kägi <kaegit@gmail.com>
2020-01-28 21:55:20 +0100

    Add first benchmark tests.

---
+++ TypeDictionaryPerformanceTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using BenchmarkDotNet.Running;
+using TypeDictionary;
+
+namespace TypeDictionaryPerformanceTest
+{
+    static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var summaries = new[]{
+                BenchmarkRunner.Run<TypeDictBenchmark<TypeDictionary<Object>>>(),
+                BenchmarkRunner.Run<TypeDictBenchmark<TypeDictionaryC<Object>>>(),
+                BenchmarkRunner.Run<TypeDictBenchmark<TypeDictionaryD<Object>>>()};
+        }
+    }
+}
---
+++ TypeDictionaryPerformanceTest/TypeDictBenchmark.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using BenchmarkDotNet.Attributes;
+using TypeDictionary;
+
+namespace TypeDictionaryPerformanceTest
+{
+    public abstract class TypeDictBenchmark<TDict> where TDict : ITypeDictionary<Object>, new()
+    {
+        private const Int32 Size = 4 * 1024;
+        private static readonly Int32[] _valueData = Enumerable.Range(0, Size).ToArray();
+        private static readonly String[] _referenceData = Enumerable.Range(0, Size).Select(n => n.ToString()).ToArray();
+        private readonly TDict _dict;
+        protected TypeDictBenchmark() => _dict = new TDict();
+        [Benchmark]
+        public void AddValueType() => Add(_valueData);
+        [Benchmark]
+        public void AddReferenceType() => Add(_referenceData);
+        [Benchmark]
+        public void AddInterleavedTypes()
+        {
+            for(Int32 i = 0; i < _referenceData.Length; ++i) {
+                _dict.Add(i);
+                _dict.Add(_referenceData[i]);
+            }
+            _dict.Clear();
+        }
+        private void Add<TSub>(IEnumerable<TSub> data)
+        {
+            foreach(TSub datum in data) {
+                _dict.Add(datum);
+            }
+            _dict.Clear();
+        }
+    }
+}
